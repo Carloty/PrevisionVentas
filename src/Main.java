f0c9aa6b7141eef3236af12db9f8b6c567bef0ad
Céline <celine.leduc@insa-rouen.fr>
2015-12-10 11:33:07 +0100

    Handle ParseException in Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,30 +11,36 @@ public class Main {
 
 	static List<RegressionTree> population;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// Execution parameters
 		int numExecutions = 5;
-		int populationSize = 15; 
-		int treeDepth = 10;
+		int populationSize = 50; 
+		int treeDepth = 5;
 		double initialMutation = 0.33;
 		int selectivePressure = 3;
-		int stopCriteria = 30;
+		int stopCriteria = 50;
 
 		// Dataset
 		HashMap<Integer, Attribute> allAttributes = Parser.getAllAttributes();
 		
 		/*
 		 * Quit the attributes that aren't correctly codified
-		 */
+		
+		allAttributes.remove(1);
+		allAttributes.remove(2);
+		allAttributes.remove(3);
+		allAttributes.remove(4);
+		allAttributes.remove(5);
+		allAttributes.remove(6);
+		allAttributes.remove(7);
 		allAttributes.remove(8);
-		allAttributes.remove(9);
 		allAttributes.remove(10);
-		allAttributes.remove(11);
-		allAttributes.remove(16);
+		allAttributes.remove(13);
+		allAttributes.remove(14);*/
 		
 		double[][] allData = Parser.getDataFromFile("age_pr2_without.csv");
-		double[][] dataTrain = allData;
-		//double[][] dataTrain = Parser.getNSamples(600000, allData);
+		//double[][] dataTrain = allData;
+		double[][] dataTrain = Parser.getNSamples(6000, allData);
 		/*
 		 * TODO modifyAttributes
 		 */
@@ -68,6 +75,7 @@ public class Main {
 		// Test of the best trees
 		int i = 0;
 		for (RegressionTree tree : bests) {
+			tree.reinitializeFitness();
 			double fitness = tree.getEvaluation(allData);
 			VisualTree treeV = new VisualTree("Tree " + i + " | " + fitness, tree);
 			treeV.printTree();
@@ -76,7 +84,7 @@ public class Main {
 
 	}
 
-	private static RegressionTree execute(HashMap<Integer,Attribute> allAttributes, double[][] dataTrain, int populationSize, double initialMutation,int selectivePressure, int stopCriteria) {
+	private static RegressionTree execute(HashMap<Integer,Attribute> allAttributes, double[][] dataTrain, int populationSize, double initialMutation,int selectivePressure, int stopCriteria) throws ParseException {
 		List<RegressionTree> fittest;
 		int i = 0;
 		double mutationPercentage;
@@ -94,13 +102,14 @@ public class Main {
 			} else {
 				counter ++;
 			}
-			/*
+			
 			for (RegressionTree tree : population) {
+				/*
 				VisualTree treeV = new VisualTree("Tree", tree);
-				treeV.printTree();
+				treeV.printTree();*/
 				System.out.println(tree.getEvaluation(dataTrain));
 			}
-			*/
+			
 			System.out.println("Best individual : " + fittest.get(0).getEvaluation(dataTrain));
 			mutationPercentage = initialMutation;
 			//mutationPercentage = (initialMutation/2) * (1 + Math.tanh((1.5*populationSize - i)/(populationSize/3)));
@@ -114,7 +123,7 @@ public class Main {
 
 	}
 
-	private static void computeIteration(double mutationPercentage, double selectivePressure, double[][] data) {
+	private static void computeIteration(double mutationPercentage, double selectivePressure, double[][] data) throws ParseException {
 		List<Integer> fittestIndexes = GeneticProgramming.selection(population, selectivePressure, data);
 		/*
 		List<RegressionTree> children = GeneticProgramming.combination(population, fittestIndexes);
@@ -124,11 +133,13 @@ public class Main {
 			children.add(population.get(selected).copy());
 		}
 		GeneticProgramming.mutation(children, mutationPercentage);
-		/*
+		
 		for (RegressionTree child : children) {
+			/*
 			VisualTree treeC = new VisualTree("Child", child);
-			treeC.printTree();
-		}*/
+			treeC.printTree();*/
+			System.out.println(child.getEvaluation(data));
+		}
 		population = GeneticProgramming.replacement(population, children, data);
 	}
 
