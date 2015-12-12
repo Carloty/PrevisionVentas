285586b8b98eeac787d41ae82cad30937cebc007
Charlotte <charlotte.touchard@insa-rouen.fr>
2015-12-12 12:40:18 +0100

    Parser : adding function to import data from the initial CSV file (the one with "" named age_pr2.csv) which was given to us on AG. WARNING : function does not work with the new CSV file available on AG (name : El cliente empieza a estar nervioso) because it does not contain a "cliente" field

--- src/Parser.java
+++ src/Parser.java
@@ -113,6 +113,40 @@ public class Parser {
 		}
 	}
 	
+	public static void getDataFromInitialFile(String fileName, double percentTrain, double[][] dataTrain, double[][] dataTest){
+		HashMap<Integer,Attribute> attList = getAllAttributes();
+		String line;
+		Random r = new Random();
+		
+		int indexTrain = 0;
+		int indexTest = 0;
+		
+		double[] dataSample;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+			
+			line = br.readLine(); // first line of the CSV file (we don't want it)
+			
+			while ((line = br.readLine()) != null) {
+				line = line.replace("\"","");
+				dataSample = Parser.transformCSVdataToDouble(line, attList);
+				double proba = r.nextDouble();
+				if(proba < percentTrain && indexTrain < dataTrain.length || proba >= percentTrain && indexTest >= dataTest.length){
+					dataTrain[indexTrain++] = dataSample;
+				} else {
+					dataTest[indexTest++] = dataSample;
+				}
+			}
+			
+			br.close();
+
+		} catch (Exception e) {
+			System.err.println("ERROR WHILE GETTING LINES FROM FILE WITH TEST/TRAIN");
+			e.printStackTrace();
+		}
+	}
+	
 	public static double[] transformCSVdataToDouble(String s, HashMap<Integer,Attribute> attList){
 		double[] data = new double[16];
 		String[] attributes = s.split(",");
